c6de7b7c51dbf74946f156c4330a48dba9ec497a
Zachary Chai <zachary.chai@younility.com>
2015-01-18 12:58:24 -0500

    change names

--- src/com/yahtzee/model/Combinations.java
+++ src/com/yahtzee/model/Combinations.java
@@ -5,23 +5,19 @@ import java.util.Set;
 
 public final class Combinations {
 	
-	private Combinations() {
-		
-	}
-	
-	public static boolean isYahtzee(MainDice dice) {
+	public static boolean verifyYahtzee(MainDice dice) {
 		Set<Die> set = new HashSet<Die>(dice.getDice());
 		return set.size() == 1;
 	}
 	
-	public static boolean isLargeStraight(MainDice dice) {
+	public static boolean verifyLargeStraight(MainDice dice) {
 		Set<Die> set = new HashSet<Die>(dice.getDice());
 		if(dice.contains(1) && dice.contains(6))
 			return false;
 		return dice.size() == set.size();
 	}
 	
-	public static boolean isSmallStraight(MainDice dice) {
+	public static boolean verifySmallStraight(MainDice dice) {
 		if(!(dice.contains(3) && dice.contains(4)))
 			return false;
 		if(dice.contains(2) && dice.contains(5))
@@ -33,7 +29,7 @@ public final class Combinations {
 		return false;
 	}
 	
-	public static boolean isFullHouse(MainDice dice) {
+	public static boolean verifyFullHouse(MainDice dice) {
 		Set<Die> set = new HashSet<Die>(dice.getDice());
 		if(set.size() != 2)
 			return false;
@@ -41,7 +37,7 @@ public final class Combinations {
 		return (dice.numDie(arr[0]) == 2 || dice.numDie(arr[0]) == 3);
 	}
 	
-	public static boolean isFourOfKind(MainDice dice) {
+	public static boolean verifyFourOfKind(MainDice dice) {
 		Set<Die> set = new HashSet<Die>(dice.getDice());
 		if(set.size() != 2)
 			return false;
@@ -49,7 +45,7 @@ public final class Combinations {
 		return (dice.numDie(arr[0]) == 4 || dice.numDie(arr[1]) == 4);
 	}
 	
-	public static boolean isThreeOfKind(MainDice dice) {
+	public static boolean verifyThreeOfKind(MainDice dice) {
 		Set<Die> set = new HashSet<Die>(dice.getDice());
 		if(set.size() > 3)
 			return false;
